74533e96f274704b8b2dd0dbe59b93f21376a9e2
aayanshchandra2-boop <aayanshchandra2@gmail.com>
2026-06-17 18:47:34 +0300

    checking the age to enroll the class

---
+++ age_checker.py
@@ -0,0 +1,11 @@
+age=int(input("enter your age between (10-20): "))
+
+if age<=20:
+    print("You are in the eligible age group (10-20)")
+else:
+    print("You are not in the eligible age group (10-20)")
+if age>=20:
+    print("you are not allowed to enroll the class")
+else:
+    print("you are allowed to enroll the class")
+
